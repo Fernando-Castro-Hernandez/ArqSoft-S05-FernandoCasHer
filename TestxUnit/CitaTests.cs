4d441a5c348b5151669be6e1c563293e0cf6ecc5
Fernando-Castro-Hernandez <jesus.castro@tecdesoftware.edu.mx>
2026-07-21 15:08:26 -0600

    test: Confirmacion de los test a la normalidad

--- TestxUnit/CitaTests.cs
+++ TestxUnit/CitaTests.cs
@@ -12,7 +12,7 @@ namespace Citas_App.Tests
             var cita = new Cita();
 
             // Assert
-            Assert.Equal("PRUEBA", cita.Estado);
+            Assert.Equal("Pendiente", cita.Estado);
         }
 
         [Fact]
